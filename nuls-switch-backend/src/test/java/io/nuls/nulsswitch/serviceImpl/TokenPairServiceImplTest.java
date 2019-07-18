3250b323fc99244fb3ddc0cb1b6391a798633ff6
Edward <chenxue@dadingsoft.com>
2019-07-18 20:40:35 +0800

    change the mybatis plus configuration

---
+++ nuls-switch-backend/src/test/java/io/nuls/nulsswitch/serviceImpl/TokenPairServiceImplTest.java
@@ -0,0 +1,28 @@
+package io.nuls.nulsswitch.serviceImpl;
+
+import io.nuls.nulsswitch.NulsSwitchApplication;
+import io.nuls.nulsswitch.service.TokenPairService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = NulsSwitchApplication.class)
+public class TokenPairServiceImplTest {
+    @Autowired
+    TokenPairService tokenPairService;
+
+    @Test
+    public void test(){
+
+        tokenPairService.selectAll();
+
+    }
+
+
+
+}
