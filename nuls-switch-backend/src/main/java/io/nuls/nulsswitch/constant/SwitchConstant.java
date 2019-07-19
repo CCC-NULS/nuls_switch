68eea9ddb7b66aaf51d39324071b51844510e8f0
qinyifeng <376000410@qq.com>
2019-07-19 13:54:48 +0800

    增加常量类

---
+++ nuls-switch-backend/src/main/java/io/nuls/nulsswitch/constant/SwitchConstant.java
@@ -0,0 +1,25 @@
+package io.nuls.nulsswitch.constant;
+
+/**
+ * 类描述：常量类
+ *
+ * @author qinyifeng
+ * @version v1.0
+ * @date 2019/7/18 21:23
+ */
+public class SwitchConstant {
+    /** 订单相关 */
+    /**
+     * 订单类型：1-买入、2-卖出
+     */
+    public final static int TX_TYPE_BUY = 1;
+    public final static int TX_TYPE_SELL = 2;
+
+    /**
+     * 状态：0-未交易、1-部分交易、2-完成交易、9-撤销
+     */
+    public final static int TX_ORDER_STATUS_INIT = 0;
+    public final static int TX_ORDER_STATUS_PART = 1;
+    public final static int TX_ORDER_STATUS_DONE = 2;
+    public final static int TX_ORDER_STATUS_CANCEL = 9;
+}
---
+++ nuls-switch-backend/src/main/java/io/nuls/nulsswitch/web/dto/order/QueryOrderReqDto.java
@@ -0,0 +1,59 @@
+package io.nuls.nulsswitch.web.dto.order;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.nuls.nulsswitch.web.dto.BaseQuery;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * 类描述：订单查询请求
+ *
+ * @author qinyifeng
+ * @version v1.0
+ * @date 2019/7/18 21:40
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@ApiModel
+public class QueryOrderReqDto extends BaseQuery {
+
+	private static final long serialVersionUID = 3967075132487249652L;
+
+	/**
+	 * 原token
+	 */
+	@ApiModelProperty(value = "原token")
+	private Integer fromTokenId;
+	/**
+	 * 目标Token
+	 */
+	@ApiModelProperty(value = "目标Token")
+	private Integer toTokenId;
+
+	/**
+	 * 用户地址
+	 */
+	@ApiModelProperty(value = "用户地址")
+	private String address;
+
+	/**
+	 * 开始时间
+	 */
+	@ApiModelProperty(value = "开始时间")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date startQueryTime;
+
+	/**
+	 * 结束时间
+	 */
+	@ApiModelProperty(value = "结束时间")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	private Date endQueryTime;
+}
